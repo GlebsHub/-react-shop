4ccbeab12f028acfbc274aa5b8cb81371fa6472a
GlebsHub <gorbunovg@inbox.ru>
2023-01-03 11:26:35 +0300

    gh-paged added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,11 @@ const Header = () =>{
     return(
         <nav>
             <div className="nav-wrapper #00bcd4 cyan">
-                <a href="#" className="brand-logo">Logo</a>
+                <a href="#" className="brand-logo">React-Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li>
+                        <a href="https://github.com/GlebsHub/-react-shop" target="_blank" rel ="noreferrer">React-Shop</a>
+                    </li>
                 </ul>
             </div>
         </nav>
